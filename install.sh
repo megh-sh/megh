c37ab212697b281f20604684c5164485c8a02ab4
Tareq Hasan <tareq1988@gmail.com>
2020-12-18 09:56:10 +0800

    Updated install script

--- install.sh
+++ install.sh
@@ -33,11 +33,55 @@ function install_php() {
 }
 
 function install_composer() {
-    curl -sSL "https://getcomposer.org/installer" | php && mv composer.phar /usr/local/bin/composer
+    if [ ! -f /usr/local/bin/composer ]; then
+        curl -sSL "https://getcomposer.org/installer" | php
+        mv composer.phar /usr/local/bin/composer
+    fi
+}
+
+function setup_firewall() {
+    ufw allow 22
+    ufw allow 80
+    ufw allow 443
+    ufw --force enable
+}
+
+function create_user() {
+    # Add sudo user and grant privileges
+    useradd --create-home --shell "/bin/bash" --groups sudo megh
+
+    # Setup bash profiles
+    cp /root/.profile /home/megh/.profile
+    cp /root/.bashrc /home/megh/.bashrc
+
+    # Create SSH key
+    mkdir -p /home/megh/.ssh
+    cp /root/.ssh/authorized_keys /home/megh/.ssh/authorized_keys
+
+    ssh-keygen -f /home/megh/.ssh/id_rsa -t rsa -N ''
+
+    # Fix Directory Permissions
+    chown -R megh:megh /home/megh
+    chmod -R 755 /home/megh
+    chmod 700 /home/megh/.ssh/id_rsa
+
+    # Add docker permissions
+    groupadd docker
+    usermod -aG docker megh
+    chmod 666 /var/run/docker.sock
+    systemctl restart docker
+
+    # Login as megh
+    su -s megh
 }
 
 function install_megh() {
     composer global require megh/megh-cli
+
+    echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >>~/.bashrc
+    source ~/.bashrc
+
+    megh install
 }
 
 # Run the commands
@@ -45,4 +89,5 @@ bootstrap
 install_docker
 install_php
 install_composer
+create_user
 install_megh
